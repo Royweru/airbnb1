c7718630226c401bdca09e1dfb3984dec76f239f
Royweru <royezi17@gmail.com>
2023-10-04 08:01:47 +0300

    new file:   actions/getFavorites.ts 	new file:   app/favorites/FavoritesClient.tsx 	modified:   app/favorites/page.tsx

---
+++ actions/getFavorites.ts
@@ -0,0 +1,31 @@
+import prisma from '@/libs/prismadb'
+
+import getCurrentUser from '@/session/GetCurrentUser'
+
+
+
+export default async function getFavoritesListing() {
+    try {
+        const currentUser = await getCurrentUser()
+
+        if(!currentUser){
+            return []
+        }
+        const favorites = await prisma.listing.findMany(
+            {
+                where:{
+                    id:{
+                        in:[...(currentUser.favouriteIds || [])]
+                    }
+                }
+            }
+        )
+        const safeFavorites = favorites.map(favorite=>({
+            ...favorite,
+            createdAt:favorite.createdAt.toISOString()
+        }))
+       return safeFavorites
+    } catch (error:any) {
+        throw new Error(error)
+    }
+}
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -3,16 +3,33 @@ import EmptyState from '../components/EmptyState'
 import Clientonly from '../components/Clientonly'
 
 import getCurrentUser from '@/session/GetCurrentUser'
+import getFavoritesListing from '@/actions/getFavorites'
+import FavoritesClient from './FavoritesClient'
 
-const favoritesPage = () => {
-  return (
-    <Clientonly>
-        <EmptyState
-          title='No favorites found'
-          subtitle='looks like you have no favorites listing'
-          />
-    </Clientonly>
-  )
+const favoritesPage = async() => {
+
+    const listings = await getFavoritesListing()
+    const currrentUser = await getCurrentUser()
+
+    if(listings.length===0){
+        return (
+            <Clientonly>
+                <EmptyState
+                  title='No favorites found'
+                  subtitle='looks like you have no favorites listing'
+                  />
+            </Clientonly>
+          )
+    }
+  
+    return(
+        <Clientonly>
+            <FavoritesClient
+               listings={listings}
+               currentUser={currrentUser}
+            />
+        </Clientonly>
+    )
 }
 
 export default favoritesPage
